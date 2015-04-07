184e3d6f0705d148b8a96d2663f64c209fa82afb
pemaxim <maxim.peshkov@usabilitydynamics.com>
2015-04-07 18:24:28 +0300

    implemented UI to manage custom property taxonomies

--- lib/classes/class-terms-bootstrap.php
+++ lib/classes/class-terms-bootstrap.php
@@ -16,7 +16,7 @@ namespace UsabilityDynamics\WPP {
        * @protected
        * @static
        * @property $instance
-       * @type UsabilityDynamics\WPP\Terms_Bootstrap object
+       * @type \UsabilityDynamics\WPP\Terms_Bootstrap object
        */
       protected static $instance = null;
       
@@ -24,9 +24,115 @@ namespace UsabilityDynamics\WPP {
        * Instantaite class.
        */
       public function init() {
-        
-        //** Here is we go. */
-        
+
+        if( !class_exists( '\UsabilityDynamics\Settings' ) ) {
+          $this->errors->add( __( 'Class \UsabilityDynamics\Settings is undefined.', $this->domain ) );
+          return;
+        }
+
+        /** Add Terms UI on Settings Developer Tab. */
+        if( current_user_can( 'manage_wpp_categories' ) ) {
+
+          add_filter( 'wpp::settings_developer::tabs', function( $tabs ){
+            $tabs['terms'] = array(
+              'label' => __( 'Taxonomies', $this->domain ),
+              'template' => $this->path( 'static/views/admin/settings-developer-terms.php', 'dir' ),
+              'order' => 25
+            );
+            return $tabs;
+          } );
+
+          add_action( 'wpp::save_settings', array( $this, 'save_terms' ) );
+
+        }
+
+        /** Define our custom taxonomies. */
+        add_filter( 'wpp_taxonomies', array( $this, 'define_taxonomies' ) );
+
+        /** Prepare taxonomy's arguments before registering taxonomy. */
+        add_filter( 'wpp::register_taxonomy', array( $this, 'prepare_taxonomy' ), 99, 2 );
+
+      }
+
+      /**
+       * Save custom Taxonomies
+       *
+       */
+      public function save_terms( $data ) {
+        if( !empty( $data[ 'wpp_terms' ] ) && is_array( $data[ 'wpp_terms' ] ) ) {
+          $taxonomies = array();
+
+          foreach( $data[ 'wpp_terms' ] as $taxonomy => $v ) {
+
+            /* Ignore missed Taxonomy */
+            if( empty( $v[ 'label' ] ) && count( $data[ 'wpp_terms' ] ) == 1 ) {
+              break;
+            }
+
+            $taxonomies[ $taxonomy ] = $this->prepare_taxonomy( $v, $taxonomy );
+
+          }
+
+          $this->set( 'config.taxonomies', $taxonomies );
+
+          $this->settings->commit();
+        }
+      }
+
+      /**
+       * Define our custom taxonomies on wpp_taxonomies hook
+       *
+       */
+      public function define_taxonomies( $taxonomies ) {
+
+        /** Init Settings */
+        $this->settings = new \UsabilityDynamics\Settings( array(
+          'key'  => 'wpp_terms',
+          'store'  => 'options',
+          'data' => array(
+            'name' => $this->name,
+            'version' => $this->args[ 'version' ],
+            'domain' => $this->domain,
+          )
+        ));
+
+        /** Be sure that we have any taxonomy to register. If not, we set default taxonomies of WP-Property. */
+        if( !$this->get( 'config.taxonomies' ) ) {
+          $this->set( 'config.taxonomies', $taxonomies );
+        }
+
+        return $this->get( 'config.taxonomies', array() );
+      }
+
+      /**
+       * Prepare arguments
+       */
+      public function prepare_taxonomy( $args, $taxonomy ) {
+
+        $args = wp_parse_args( $args, array(
+          'label' => $taxonomy,
+          'labels' => array(),
+          'public' => false,
+          'hierarchical' => false,
+          'show_ui' => false,
+          'show_in_nav_menus' => false,
+          'show_tagcloud' => false,
+          'capabilities' => array(
+            'manage_terms' => 'manage_wpp_categories',
+            'edit_terms'   => 'manage_wpp_categories',
+            'delete_terms' => 'manage_wpp_categories',
+            'assign_terms' => 'manage_wpp_categories'
+          ),
+        ) );
+
+        /* May be fix data type */
+        foreach( $args as &$arg ) {
+          if( is_string( $arg ) && $arg === 'true' ) {
+            $arg = true;
+          }
+        }
+
+        return $args;
       }
       
       /**
---
+++ static/views/admin/settings-developer-terms.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Settings 'Developer' Tab
+ * Section 'Advanced'
+ */
+
+?>
+<h3><?php printf( __( '%1s Taxonomies', 'wpp' ), WPP_F::property_label() ); ?></h3>
+<p><?php printf( __( 'Manage your %s Taxonomies here. Note, you can not remove all taxonomies, in this case default WP-Property taxonomies will be returned back.', ud_get_wpp_terms()->domain ), WPP_F::property_label() ); ?></p>
+
+<table id="" class="ud_ui_dynamic_table widefat">
+  <thead>
+  <tr>
+    <th class='wpp_draggable_handle'>&nbsp;</th>
+    <th class='wpp_attribute_name_col'><?php _e( 'Label', 'wpp' ) ?></th>
+    <th class='wpp_attribute_slug_col'><?php _e( 'Slug', 'wpp' ) ?></th>
+    <th class='wpp_settings_col'><?php _e( 'Settings', 'wpp' ) ?></th>
+    <th class='wpp_delete_col'>&nbsp;</th>
+  </tr>
+  </thead>
+  <tbody>
+  <?php foreach( ud_get_wpp_terms( 'config.taxonomies', array() ) as $slug => $data ): ?>
+    <?php $data = ud_get_wpp_terms()->prepare_taxonomy( $data, $slug ); ?>
+    <tr class="wpp_dynamic_table_row" slug="<?php echo $slug; ?>" new_row='false'>
+      <th class='wpp_draggable_handle'>&nbsp;</th>
+      <td>
+        <ul>
+          <li>
+            <input class="slug_setter" type="text" name="wpp_terms[<?php echo $slug; ?>][label]" value="<?php echo $data['label']; ?>"/>
+          </li>
+          <li class="hide-on-new-row">
+            <a href="<?php echo admin_url( "edit-tags.php?taxonomy={$slug}&post_type=property" ); ?>"><?php _e( 'Manage Terms', ud_get_wpp_terms()->domain ); ?></a>
+          </li>
+        </ul>
+      <td>
+        <ul>
+          <li>
+            <input type="text" class="slug" readonly='readonly' value="<?php echo $slug; ?>"/>
+          </li>
+        </ul>
+      </td>
+      <td>
+        <ul>
+          <li>
+            <label><input type="checkbox" name="wpp_terms[<?php echo $slug; ?>][hierarchical]" <?php checked( $data['hierarchical'], true ); ?> value="true"/> <?php _e( 'Hierarchical', ud_get_wpp_terms()->domain ); ?></label>
+          </li>
+          <li>
+            <label><input type="checkbox" name="wpp_terms[<?php echo $slug; ?>][public]" <?php checked( $data['public'], true ); ?> value="true"/> <?php _e( 'Public', ud_get_wpp_terms()->domain ); ?></label>
+          </li>
+          <li>
+            <label><input type="checkbox" name="wpp_terms[<?php echo $slug; ?>][show_ui]" <?php checked( $data['show_ui'], true ); ?> value="true"/> <?php _e( 'Show UI', ud_get_wpp_terms()->domain ); ?></label>
+          </li>
+          <li>
+            <label><input type="checkbox" name="wpp_terms[<?php echo $slug; ?>][show_in_nav_menus]" <?php checked( $data['show_in_nav_menus'], true ); ?> value="true"/> <?php _e( 'Show in Nav Menus', ud_get_wpp_terms()->domain ); ?></label>
+          </li>
+          <li>
+            <label><input type="checkbox" name="wpp_terms[<?php echo $slug; ?>][show_tagcloud]" <?php checked( $data['show_tagcloud'], true ); ?> value="true"/> <?php _e( 'Show in Tag Cloud', ud_get_wpp_terms()->domain ); ?></label>
+          </li>
+        </ul>
+      </td>
+
+      <td>
+        <span class="wpp_delete_row wpp_link"><?php _e( 'Delete', ud_get_wpp_terms()->domain ); ?></span>
+      </td>
+    </tr>
+
+  <?php endforeach; ?>
+  </tbody>
+
+  <tfoot>
+  <tr>
+    <td colspan='4'>
+      <input type="button" class="wpp_add_row button-secondary" value="<?php _e( 'Add Row', 'wpp' ) ?>"/>
+    </td>
+  </tr>
+  </tfoot>
+
+</table>
