f41f135c4038d888691e7fab896a9e6e55d625e4
pemaxim <maxim.peshkov@usabilitydynamics.com>
2015-05-19 18:27:46 +0300

    fixed strict standards

--- vendor/plugins/wp-tax-post-binding/cf-tax-post-binding.php
+++ vendor/plugins/wp-tax-post-binding/cf-tax-post-binding.php
@@ -568,7 +568,7 @@ jQuery(document).ready(function($) {
 		return $thumbnail;
 	}
 	
-	public function handle_taxonomy_archive($posts, &$query) {
+	public static function handle_taxonomy_archive($posts, &$query) {
 		if ($query->is_main_query() && $query->is_page() && empty($posts) && $post_type_archive = self::supports($query->get('pagename'))) {
 			global $wp_the_query, $wp_query;
 			// We want the "taxonomy" archive, which should actually be the hidden post-type archive.
@@ -599,7 +599,7 @@ jQuery(document).ready(function($) {
 		return $posts;
 	}
 	
-	public function handle_taxonomy_archive_wp_title($title, $sep, $seplocation) {
+	public static function handle_taxonomy_archive_wp_title($title, $sep, $seplocation) {
 		if (is_main_query() && is_tax() && is_archive()) {
 			$obj = get_queried_object();
 			if (empty($obj->taxonomy) && !empty($obj->label)) {
@@ -615,7 +615,7 @@ jQuery(document).ready(function($) {
 		return $title;
 	}
 	
-	public function override_taxonomy_pagination() {
+	public static function override_taxonomy_pagination() {
 		// We're doing this because we don't want taxonomy base pages to use normal pagination, so we're just overriding the parameter.
 		if (is_main_query() && is_tax() && is_archive()) {
 			$obj = get_queried_object();
@@ -632,19 +632,20 @@ jQuery(document).ready(function($) {
 		}
 	}
 	
-	public function readd_paged_var() {
+	public static function readd_paged_var() {
 		global $wp_query;
 		$wp_query->set('paged', self::$_stored_paged);
 		remove_action('template_redirect', 'cf_taxonomy_post_type_binding::readd_paged_var', 1000);
 	}
 	
-	public function handle_taxonomy_archive_first_page_link($link_text) {
+	public static function handle_taxonomy_archive_first_page_link($link_text) {
 		if (empty($link_text)) {
 			$link_text = remove_query_arg('paged', $_SERVER['REQUEST_URI']);
 		}
 		return $link_text;
 	}
 }
+
 add_filter('cffim_item_thumbnail', 'cf_taxonomy_post_type_binding::cffim_item_thumbnail', 10, 3);
 add_filter('post_type_link', 'cf_taxonomy_post_type_binding::post_link', 10, 2);
 add_action('wp_loaded', 'cf_taxonomy_post_type_binding::on_wp_loaded');
