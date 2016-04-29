9fbaab3615c4eea9d88eacc71f168929aca09ede
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2016-04-29 18:52:12 +0600

    issee #11

--- changes.md
+++ changes.md
@@ -1,6 +1,7 @@
 ### 1.0.2 (  )
 * Added Add-on's settings to Backup of Current WP-Property Configuration.
 * Fixed compatibility with Mandrill plugin.
+* "Show in Admin Menu and add native Meta Box" splitted into two separated options. New options are "Show in Admin Menu" and "Add native Meta Box".
 
 ### 1.0.1 ( October 7, 2015 )
 * Updated plugin initialisation logic.
--- lib/classes/class-terms-upgrade.php
+++ lib/classes/class-terms-upgrade.php
@@ -20,14 +20,12 @@ namespace UsabilityDynamics\WPP {
       static public function run( $old_version, $new_version ){
         global $wp_properties;
 
-        self::do_backup( $old_version, $new_version );
-
         /**
          * Specific upgrade conditions.
          */
         switch( true ) {
 
-          case ( version_compare( $old_version, '1.0.4', '<' ) ):
+          case ( version_compare( $old_version, '1.0.2', '<' ) ):
             $taxonomies = ud_get_wpp_terms()->define_taxonomies( array() );
             //$settings = ud_get_wpp_terms()->get( 'config.taxonomies', array() );
             if( is_array( $taxonomies ) ) {
@@ -50,47 +48,6 @@ namespace UsabilityDynamics\WPP {
         }
         /* Additional stuff can be handled here */
         do_action( ud_get_wpp_terms()->slug . '::upgrade', $old_version, $new_version );
-      }
-
-      /**
-       * Saves backup of WPP settings to uploads and to DB.
-       *
-       * @param $old_version
-       * @param $new_version
-       */
-      static public function do_backup( $old_version, $new_version ) {
-        /* Do automatic Settings backup! */
-        global $wp_properties;
-        if (empty($wp_properties)) {
-          WPP_F::settings_action(true);
-        }
-
-        if( !empty( $wp_properties ) ) {
-
-          /**
-           * Fixes allowed mime types for adding download files on Edit Product page.
-           *
-           * @see https://wordpress.org/support/topic/2310-download-file_type-missing-in-variations-filters-exe?replies=5
-           * @author peshkov@UD
-           */
-          add_filter( 'upload_mimes', function( $t ){
-            if( !isset( $t['json'] ) ) {
-              $t['json'] = 'application/json';
-            }
-            return $t;
-          }, 99 );
-
-          $filename = md5( 'wpp_settings_backup' ) . '.json';
-          $upload = wp_upload_bits( $filename, null, json_encode( $wp_properties ) );
-
-          if( !empty( $upload ) && empty( $upload[ 'error' ] ) ) {
-            if( isset( $upload[ 'error' ] ) ) unset( $upload[ 'error' ] );
-            $upload[ 'version' ] = $old_version;
-            $upload[ 'time' ] = time();
-            update_option( 'wpp_settings_backup', $upload );
-          }
-
-        }
       }
 
     }
